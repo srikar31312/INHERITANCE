78172fc7f8f19a1f38f0aa2150abe9824b54c72e
srikar31312 <vadlurisrikar@gmail.com>
2025-01-15 09:45:06 +0530

    acp

---
+++ activity 3.py
@@ -0,0 +1,32 @@
+# parent class
+class Bird:
+
+    def  __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim faster")
+
+# chils class
+class Penguin(Bird):
+
+    def __init__(self):
+        # call super()function
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisTHis(self):
+        print("Penguin")
+        
+    def run(self):
+        print("Run faster")
+
+
+# Object creation
+peggy = Penguin()
+peggy.whoisTHis()
+peggy.swim()
+peggy.run
